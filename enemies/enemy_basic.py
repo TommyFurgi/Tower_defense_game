a2ac9c3a65a45c7c41cf98dd18278fe31a79fafd
TommyFurgi <tomcio.220903@gmail.com>
2024-05-02 13:45:02 +0200

    created another enemy class

--- enemies/enemy_basic.py
+++ enemies/enemy_basic.py
@@ -1,9 +1,100 @@
 from enemies.enemy import Enemy
+import pygame
+from PIL import Image
+from directions import Direction
+from effects.effect_type import EffectType
+import random
 
 class EnemyBasic(Enemy):
     
     def __init__(self):
-        
         Enemy.__init__(self, "assets/enemies/enemy.png")
+
+        # starting point
+        self.x = 540
+        self.y = 900
+        self.rect = pygame.Rect(self.x, self.y, 64, 64) # Required in order for collisions to work
+
+        self.path = [
+            (540, 900), (539, 892), (541, 868), (543, 837), (547, 806), (555, 771),
+            (570, 750), (587, 732), (607, 717), (632, 706), (659, 699), (677, 693),
+            (692, 667), (699, 638), (702, 597), (695, 569), (688, 544), (676, 530),
+            (660, 515), (646, 503), (627, 496), (606, 493), (574, 479), (550, 470),
+            (523, 457), (501, 429), (481, 409), (472, 383), (470, 354), (462, 319),
+            (467, 289), (475, 263), (487, 240), (503, 229), (525, 214), (547, 207),
+            (573, 205), (595, 204), (615, 201), (637, 200), (680, 199), (717, 197),
+            (752, 198), (787, 197), (816, 198), (853, 199), (895, 195), (935, 194),
+            (979, 193), (1019, 194), (1055, 193), (1098, 191), (1147, 192),
+            (1195, 190), (1246, 191), (1289, 190), (1329, 191), (1353, 192)
+        ]
+        self.speed = 1.2
+        self.max_speed = 1.2
+        self.health = 200
+        self.max_health = 200
+        self.reward = 30
+
         
+
+    
+    def load_images(self, images_filename):
         
+        self.animation_count = random.randint(0, 7) * 10
+        self.path_pos = 0
+        self.imgs_up = []
+        self.imgs_down = []
+        self.imgs_right = []
+        self.imgs_left = []
+        
+        animation_strip = Image.open(images_filename)
+        frame_width = 64
+        frame_height = 64
+        
+        for i in range(4):
+            for j in range(1,9):
+                frame = animation_strip.crop((frame_width * j, frame_height * i, frame_width * (j + 1), frame_height * (i+1)))
+
+                data = frame.tobytes()
+                pygame_surface = pygame.image.fromstring(data, frame.size, "RGBA")
+
+                match i:
+                    case 0:
+                        self.imgs_up.append(pygame.transform.scale(pygame_surface, (128, 128)))
+                    case 1:
+                        self.imgs_left.append(pygame.transform.scale(pygame_surface, (128, 128)))
+                    case 2:
+                        self.imgs_down.append(pygame.transform.scale(pygame_surface, (128, 128)))
+                    case 3:
+                        self.imgs_right.append(pygame.transform.scale(pygame_surface, (128, 128)))
+
+        self.direction = Direction.RIGHT
+        self.img = self.imgs_right[0]
+
+        self.flipped = False
+
+    def handle_effects(self):
+        
+        finished_effects = []
+        
+        for effect in self.effects:
+            
+            effect_type, property = effect.update()
+            
+            match effect_type:
+                case EffectType.POISION:
+                    self.lose_hp(property)
+                    #TODO: poison effect
+                case EffectType.SLOWDOWN:
+                    #TODO: add function to slow enemy down
+                    self.speed = property * self.max_speed
+                    pass
+                case EffectType.EFFECT_FINISHED: # effect duration has ended
+                    finished_effects.append(effect)
+                    
+        for finished_effect in finished_effects:
+            
+            match finished_effect.get_effect_type():
+                
+                case EffectType.SLOWDOWN:
+                    self.speed = self.max_speed # restoring speed to its original value
+            
+            self.effects.remove(finished_effect)
---
+++ enemies/enemy_magic.py
@@ -0,0 +1,100 @@
+from enemies.enemy import Enemy
+import pygame
+from PIL import Image
+from directions import Direction
+from effects.effect_type import EffectType
+import random
+
+class EnemyMagic(Enemy):
+    
+    def __init__(self):
+        Enemy.__init__(self, "assets/enemies/enemy.png")
+
+        # starting point
+        self.x = 1349
+        self.y = 755
+        self.rect = pygame.Rect(self.x, self.y, 64, 64) # Required in order for collisions to work
+        
+        self.path = [
+            (1349, 755), (1305, 755), (1268, 755), (1238, 754), (1202, 756),
+            (1167, 756), (1121, 757), (1077, 758), (1038, 757), (999, 756),
+            (972, 758), (940, 759), (888, 759), (845, 758), (802, 750),
+            (773, 734), (742, 719), (722, 701), (704, 663), (697, 627),
+            (685, 581), (666, 544), (645, 505), (594, 477), (549, 469),
+            (511, 456), (494, 426), (479, 401), (471, 345), (467, 293),
+            (494, 232), (555, 207), (606, 196), (685, 192), (736, 192),
+            (798, 187), (872, 195), (912, 197), (963, 195), (1003, 195),
+            (1052, 193), (1102, 193), (1158, 194), (1209, 196), (1253, 194),
+            (1288, 193), (1324, 190), (1353, 187), (1381, 185)
+        ]
+
+        self.speed = 1.5
+        self.max_speed = 1.5
+        self.health = 300
+        self.max_health = 300
+        self.reward = 20
+
+        
+
+    
+    def load_images(self, images_filename):
+        
+        self.animation_count = random.randint(0, 7) * 10
+        self.path_pos = 0
+        self.imgs_up = []
+        self.imgs_down = []
+        self.imgs_right = []
+        self.imgs_left = []
+        
+        animation_strip = Image.open(images_filename)
+        frame_width = 64
+        frame_height = 64
+        
+        for i in range(4,8):
+            for j in range(1,9):
+                frame = animation_strip.crop((frame_width * j, frame_height * i, frame_width * (j + 1), frame_height * (i+1)))
+
+                data = frame.tobytes()
+                pygame_surface = pygame.image.fromstring(data, frame.size, "RGBA")
+
+                match i:
+                    case 4:
+                        self.imgs_up.append(pygame.transform.scale(pygame_surface, (128, 128)))
+                    case 5:
+                        self.imgs_left.append(pygame.transform.scale(pygame_surface, (128, 128)))
+                    case 6:
+                        self.imgs_down.append(pygame.transform.scale(pygame_surface, (128, 128)))
+                    case 7:
+                        self.imgs_right.append(pygame.transform.scale(pygame_surface, (128, 128)))
+
+        self.direction = Direction.RIGHT
+        self.img = self.imgs_right[0]
+
+        self.flipped = False
+
+    def handle_effects(self):
+        
+        finished_effects = []
+        
+        for effect in self.effects:
+            
+            effect_type, property = effect.update()
+            
+            match effect_type:
+                case EffectType.POISION:
+                    self.lose_hp(property)
+                    #TODO: poison effect
+                case EffectType.SLOWDOWN:
+                    # can not be slowed down
+                    pass
+                case EffectType.EFFECT_FINISHED: # effect duration has ended
+                    finished_effects.append(effect)
+                    
+        for finished_effect in finished_effects:
+            
+            match finished_effect.get_effect_type():
+                
+                case EffectType.SLOWDOWN:
+                    self.speed = self.max_speed # restoring speed to its original value
+            
+            self.effects.remove(finished_effect)
